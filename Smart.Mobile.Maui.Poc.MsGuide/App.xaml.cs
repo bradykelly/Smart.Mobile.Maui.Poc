a1a27e96a03a7966f12f1c0051b2a2d52b8d3306
Brady Kelly <brady@bradykelly.net>
2024-03-07 23:42:25 +0200

    First. Battling with nuget

---
+++ Smart.Mobile.Maui.Poc.MsGuide/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Smart.Mobile.Maui.Poc.MsGuide;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Smart.Mobile.Maui.Poc.MsGuide/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Smart.Mobile.Maui.Poc.MsGuide;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
