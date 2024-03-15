7a7237ae2b3f6a10b76b4250b73ed1decc41621c
Brady Kelly <brady@bradykelly.net>
2024-03-15 08:47:19 +0200

    Removed config shit

--- Smart.Mobile.Maui.Poc.MsGuide/MauiProgram.cs
+++ Smart.Mobile.Maui.Poc.MsGuide/MauiProgram.cs
@@ -10,17 +10,6 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		// var asm = Assembly.GetExecutingAssembly();
-		// using var stream = asm.GetManifestResourceStream("Smart.Mobile.Maui.Poc.MsGuide.appsettings.json");
-		// var config = new ConfigurationBuilder()
-		// 	.AddJsonStream(stream)
-		// 	.Build();
-		
-		IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-			.SetBasePath(currentDirectory)
-			.AddJsonFile("appsettings.json", optional: false)
-			.Build();		
-		
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
@@ -30,8 +19,6 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-		builder.Services.Configure<MobileQuizzOptions>(builder.Configuration.GetSection(MobileQuizzOptions.ConfigurationName));
-			
 
 #if DEBUG
 		builder.Logging.AddDebug();
